af855e56f33da3f5f80007967ca47831c5dd10b9
pashaman <plavel@gmail.com>
2026-07-07 09:58:52 +0700

    fix: refactored entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -e
-touch /var/lib/diana-db/current.didb
-touch /var/lib/diana-db/users
 /usr/bin/diana-db add-user -u admin -p admin
 /usr/bin/diana-db grant-access -u admin -db test:3
 /usr/bin/diana-db-server
